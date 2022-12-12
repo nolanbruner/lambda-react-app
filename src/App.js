6a112c00866ec6815bd9101889bb64070035fe3c
nolanbruner <nolansname@aol.com>
2022-12-12 17:13:21 -0500

    added mongoDB API for Random philosophy Quote

--- src/App.js
+++ src/App.js
@@ -3,20 +3,37 @@ import './App.css';
 import React,{ useState } from 'react';
 
 function App() {
- const[state,setState] =useState({msg:"Default Message"})
+ const[state,setState] =useState({msg:"Default Message",auth:""})
   return (
     <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
        <p>Message: {state.msg}</p>
+       {/* {(state.auth!=="")?<p>Author: {state.auth}</p>:<></>} */}
        <button onClick={()=>
            fetch("/.netlify/functions/hello")
             .then(response=>response.json())
-            // .then((x)=>console.log(x))
             .then((message)=>setState(message))
       }
       >
-        Click Me
+        Hello
+        </button>
+        <button onClick={()=>
+           fetch("/.netlify/functions/test")
+            .then(response=>response.json())
+            .then((message)=>setState(message))
+      }
+      >
+       Random Dad Joke
+        </button>
+        <button onClick={()=>
+           fetch("/.netlify/functions/mongo")
+            .then(response=>response.json())
+            .then((message)=>setState(message))
+            // .then(x=>console.log(x))
+      }
+      >
+       Random Stoic Quote
         </button>
       </header>
     </div>
