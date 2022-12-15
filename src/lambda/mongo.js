adde13a00edc82c0d4901b331666bb4a1ec63e3e
nolanbruner <nolansname@aol.com>
2022-12-15 15:24:37 -0500

    fixed API issue

--- src/lambda/mongo.js
+++ src/lambda/mongo.js
@@ -2,6 +2,30 @@
 import dotenv from "dotenv"
 dotenv.config()
 
+
+// const { MongoClient } = require("mongodb");
+
+// const mongoClient = new MongoClient(process.env.URI);
+
+// const clientPromise = mongoClient.connect();
+
+// const handler = async (event) => {
+//     try {
+//         const database = (await clientPromise).db(process.env.DB);
+//         const collection = database.collection(process.env.COLLECTION);
+//         const results = await collection.find({}).limit(1).toArray();
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify({ msg: results })
+//         };
+//     } catch (error) {
+//         return { statusCode: 500, body: error.toString() }
+//     }
+// }
+
+// module.exports = { handler }
+
+
 import axios from "axios"
 export async function handler(event, context) {
     const web = process.env.MONGO_ENDPOINT
@@ -10,6 +34,8 @@ export async function handler(event, context) {
     try {
         const response = await axios.get(process.env.MONGO_ENDPOINT, {
             headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Request-Headers': ' *',
                 'api-key': process.env.KEY,
             }
         })
@@ -23,8 +49,7 @@ export async function handler(event, context) {
         console.log(err) // output to netlify function log
         return {
             statusCode: 500,
-            body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+            body: JSON.stringify({ msg: "NO MESSAGE" }) // Could be a custom message or object i.e. JSON.stringify(err)
         }
     }
 }
-
